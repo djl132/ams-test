7bf8e0218231f2c7b29666d4fee08aca87e33ac5
DerekLin <djl132@case.edu>
2017-05-15 17:14:15 -0700

    PROBLEM: comments partial not registering

--- Desktop/.idea/21.js
+++
@@ -1,3 +0,0 @@
-/**
- * Created by DerekJLin on 10/23/16.
- */
--- Desktop/bloc-chat/blocChat/app/scripts/controllers/chatController.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  function chatController(Room){
-    this.rooms = Room.all;
-  }
-  
-  angular
-    .module('blocChat')
-    .controller('chatController', ['Room', chatController]);
-})();
